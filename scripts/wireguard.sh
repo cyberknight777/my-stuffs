8db27567fbfdbb1423665088ff9c90ef399c0b94
Cyber Knight <cyberknight755@gmail.com>
2021-08-15 21:17:03 +0800

    scripts: added detection to wireguard in drivers/net
    This is needed to resolve conflicts between drivers/net/wireguard and net/wireguard as in my experience, I've seen LineageOS kernels have wireguard in drivers/net whereas most standalone kernels have it in net/.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- scripts/wireguard.sh
+++ scripts/wireguard.sh
@@ -4,6 +4,12 @@ echo -ne "\033[1;36m Provide latest version of wireguard: \033[0m"
 read -r ver
 echo -ne "\033[1;36m Provide path to kernel source: \033[0m"
 read -r path
+if [ -d "${path}/drivers/net/wireguard" ]; then
+    rm -rf ${path}/drivers/net/wireguard
+    cd ${path} || exit 1
+    git add drivers/net/wireguard
+    git commit -S -s -m "drivers/net: removed wireguard from drivers/net"
+fi
 wget https://git.zx2c4.com/wireguard-linux-compat/snapshot/wireguard-linux-compat-"${ver}".zip
 unzip wireguard-linux-compat-"${ver}".zip -d wireguard
 cp -r wireguard/src/* "${path}"/net/wireguard
