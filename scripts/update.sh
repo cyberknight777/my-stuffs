fdc855e2bc5ba6046385f2a9d6451488298b4506
Cyber Knight <cyberknight755@gmail.com>
2021-08-17 21:58:55 +0800

    scripts: refactored update.sh
    This is done to replace subtree with the older method which is manually merging and using read-tree. This helps as to provide better commit messages when one were to be using this script to add/update wlan and techpack with newer CAF tags
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,18 +2,58 @@
 # Thanks to mochi for the script :P
 
 gsa() {
-	git subtree add --prefix=drivers/staging/qcacld-3.0 https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0 $1
-	git subtree add --prefix=drivers/staging/fw-api https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/fw-api $1
-	git subtree add --prefix=drivers/staging/qca-wifi-host-cmn https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn $1
-	git subtree add --prefix=techpack/audio https://source.codeaurora.org/quic/la/platform/vendor/opensource/audio-kernel $1
-	git subtree add --prefix=techpack/data https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/data-kernel $1
+	git remote add qcacld-3.0 https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0
+	git fetch qcacld-3.0 $1
+	git merge -S -s ours --no-commit --allow-unrelated-histories --squash FETCH_HEAD
+	git read-tree --prefix=drivers/staging/qcacld-3.0 -u FETCH_HEAD
+	git commit -S -s -m "Merge tag '${1}' of https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0"
+
+	git remote add fw-api https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/fw-api
+	git fetch fw-api $1
+	git merge -S -s ours --no-commit --allow-unrelated-histories --squash FETCH_HEAD
+	git read-tree --prefix=drivers/staging/fw-api -u FETCH_HEAD
+	git commit -S -s -m "Merge tag '${1}' of https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/fw-api"
+
+	git remote add qca-wifi-host-cmn https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn
+	git fetch qca-wifi-host-cmn $1
+	git merge -S -s ours --no-commit --allow-unrelated-histories --squash FETCH_HEAD
+	git read-tree --prefix=drivers/staging/qca-wifi-host-cmn -u FETCH_HEAD
+	git commit -S -s -m "Merge tag '${1}' of https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn"
+
+	git remote add audio-kernel https://source.codeaurora.org/quic/la/platform/vendor/opensource/audio-kernel
+	git fetch audio-kernel $1
+	git merge -S -s ours --no-commit --allow-unrelated-histories --squash FETCH_HEAD
+	git read-tree --prefix=techpack/audio -u FETCH_HEAD
+	git commit -S -s -m "Merge tag '${1}' of https://source.codeaurora.org/quic/la/platform/vendor/opensource/audio-kernel"
+
+	git remote add data-kernel https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/data-kernel
+	git fetch data-kernel $1
+	git merge -S -s ours --no-commit --allow-unrelated-histories --squash FETCH_HEAD
+	git read-tree --prefix=techpack/data -u FETCH_HEAD
+	git commit -S -s -m "Merge tag '${1}' of https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/data-kernel"
+
 }
 gsp() {
-	git subtree pull --prefix=drivers/staging/qcacld-3.0 https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0 $1
-	git subtree pull --prefix=drivers/staging/fw-api https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/fw-api $1
-	git subtree pull --prefix=drivers/staging/qca-wifi-host-cmn https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn $1
-	git subtree pull --prefix=techpack/audio https://source.codeaurora.org/quic/la/platform/vendor/opensource/audio-kernel $1
-	git subtree pull --prefix=techpack/data https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/data-kernel $1
+	git fetch qcacld-3.0 $1
+	git merge -X subtree=drivers/staging/qcacld-3.0 --squash FETCH_HEAD
+	git commit -S -s -m "Merge tag '${1}' of https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0"
+
+	git fetch fw-api $1
+	git merge -X subtree=drivers/staging/fw-api --squash FETCH_HEAD
+	git commit -S -s -m "Merge tag '${1}' of https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/fw-api"
+
+	git fetch qca-wifi-host-cmn $1
+	git merge -X subtree=drivers/staging/qca-wifi-host-cmn --squash FETCH_HEAD
+	git commit -S -s -m "Merge tag '${1}' of https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn"
+
+	git fetch audio-kernel $1
+	git merge -X subtree=techpack/audio --squash FETCH_HEAD
+	git commit -S -s -m "Merge tag '${1}' of https://source.codeaurora.org/quic/la/platform/vendor/opensource/audio-kernel"
+
+	git fetch data-kernel $1
+	git merge -X subtree=techpack/data --squash FETCH_HEAD
+	git commit -S -s -m "Merge tag '${1}' of https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/data-kernel"
+
 }
 if [[ $1 != "" && $1 == "add" ]]; then
 	gsa $2
