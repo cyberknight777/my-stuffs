c34f8d4f367fe7a34b04d1e362baaa3829bfbe66
Cyber Knight <cyberknight755@gmail.com>
2021-08-26 00:15:08 +0800

    scripts: added emacs.sh
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

---
+++ scripts/emacs.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+echo -ne "\033[1;36mEnter version of emacs you would like to install \033[0m"
+read -r version
+
+mkdir -p emacs-build
+cd emacs-build
+
+lonk=https://ftp.gnu.org/gnu/emacs/emacs-$version.tar.gz
+
+wget $lonk
+tar -xvf emacs-$version.tar.gz
+
+cd emacs-$version
+ac_cv_lib_gif_EGifPutExtensionLast=yes ./configure --prefix=/usr --sysconfdir=/etc --libexecdir=/usr/lib --localstatedir=/var --with-x-toolkit=gtk3 --with-xft --with-wide-int --with-modules --with-xwidgets
+
+make -j$(nproc --all)
+
+pkexec make install PREFIX=/usr/bin
+
+echo -e "\033[1memacs-$version installed successfully!! \033[0m"
